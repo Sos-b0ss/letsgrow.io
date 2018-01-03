58cf7eff8de3b4e8d9541da774d6d03695da1ebd
Sos_b0ss <gabrielrodd@gmail.com>
2018-01-03 02:00:42 -0700

    Full Commit with React and React-DOM installed

---
+++ Web-app-modules/letsgrow/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Lets Grow io!</h1>
+        </header>
+        <p className="App-intro">
+         <style dangerouslySetInnerHTML={{__html: "\nwhite\n" }} />
+          Uhm.. This is awkward. Were still under development. Come back soon!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
